f7a7fd313f9833f876df7dac69c8bf3403b2750d
mari300 <jahfly9@gmail.com>
2016-04-05 21:16:42 +0000

    adding the rest of files

---
+++ RandomColor.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var colors = ["Blue", "Green", "Red", "Orange", "Yellow", "Black", "Purple", "White"]
+      for ( var i = 0; i < 7; i++) {
+          $(".container").append("<div> hello </div>");
+         }
+        
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
